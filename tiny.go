8a2f359ce52a268b33f27c12db43d12d3eaa3e3e
Ivan Jovanovic <ivan.jovanovic@local.ch>
2011-09-24 18:15:04 +0200

    Simple TCP server on port 8088 with dummy go proc to render dummy response

--- tiny.go
+++ tiny.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "net"
+)
+
+func serveClient(conn *net.TCPConn) {
+  message := []byte("Yep, serving from ")
+  conn.Write(message)
+  defer conn.Close()
+}
 
 func main() {
-  fmt.Println("have to setup socket first")
+
+  addr := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8088}
+  serverListener, _ := net.ListenTCP("tcp", addr)
+
+  for true {
+    conn, _ := serverListener.AcceptTCP()
+    go serveClient(conn)
+  }
 }
