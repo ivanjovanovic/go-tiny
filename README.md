806c66c2200432114234727e05a2d5400869285f
Ivan Jovanovic <ivan.jovanovic@gmail.com>
2012-01-17 00:37:21 +0100

    Defined Request type and used bufferedIO for reading the client input

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Tiny HTTP server implementation in Go
 
 Made for educational purposes. Inspired by [tiny httpd](http://tinyhttpd.sourceforge.net/)
+
+## Roadmap
+
+* Static file serving
+* MIME types based on file extension
--- tiny.go
+++ tiny.go
@@ -1,22 +1,58 @@
 package main
 
 import (
+  "os"
   "net"
+  "bufio"
 )
 
-func serveClient(conn *net.TCPConn) {
-  message := []byte("Yep, serving from ")
-  conn.Write(message)
-  defer conn.Close()
+type Request struct {
+  // request method (i.e GET, POST ...)
+  method []byte
+
+  // host
+  host []byte
+
+  // path to the resource
+  path []byte
 }
 
+/*
+ * Here starts our program
+*/
 func main() {
-
   addr := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8088}
   serverListener, _ := net.ListenTCP("tcp", addr)
 
-  for true {
+  // listens for incomming connections continuously
+  for {
     conn, _ := serverListener.AcceptTCP()
+    // sets one go routine per connection established
     go serveClient(conn)
   }
 }
+
+/*
+ * One go routing per client connection
+*/
+func serveClient(conn *net.TCPConn) {
+  defer conn.Close() // we will close the connection once we exit this function
+
+  // making buffered IO reader and simple echo server
+  var reader = bufio.NewReader(conn)
+
+  // making new request object
+  request, _ := parseRequest(reader)
+
+  // writing back to client
+  conn.Write(request.method)
+}
+
+/*
+ * Parse the client request headers and build request object.
+*/
+func parseRequest(r *bufio.Reader) (request *Request, err os.Error) {
+  return new(Request), nil
+}
+
+
