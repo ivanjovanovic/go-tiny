44f5e8b153fcb3e38f04d68a655e74f23d62b7c1
Ivan Jovanovic <ivan.jovanovic@gmail.com>
2012-01-17 23:28:53 +0100

    Made first step to parsing the request

--- tiny.go
+++ tiny.go
@@ -3,18 +3,21 @@ package main
 import (
   "os"
   "net"
+  "net/textproto"
   "bufio"
+  "strings"
+  /* "fmt"*/
 )
 
 type Request struct {
   // request method (i.e GET, POST ...)
-  method []byte
-
-  // host
-  host []byte
+  Method string
 
   // path to the resource
-  path []byte
+  Path string
+
+  // Protocl version
+  HTTPVersion string
 }
 
 /*
@@ -33,26 +36,48 @@ func main() {
 }
 
 /*
- * One go routing per client connection
+ * This methods handles client request
+ * 1. Parses the request
+ * 2. Decides on validity of request
+ * 3. Decides on the action to be taken
 */
 func serveClient(conn *net.TCPConn) {
   defer conn.Close() // we will close the connection once we exit this function
 
   // making buffered IO reader and simple echo server
-  var reader = bufio.NewReader(conn)
+  var reader = textproto.NewReader(bufio.NewReader(conn))
 
   // making new request object
-  request, _ := parseRequest(reader)
+  request, err := parseRequest(reader)
+  
+  if err != nil {
+    conn.Write([]byte("Invalid request"))
+    return
+  }
 
   // writing back to client
-  conn.Write(request.method)
+  conn.Write([]byte(request.Method))
+  conn.Write([]byte(request.Path))
+  conn.Write([]byte(request.HTTPVersion))
 }
 
 /*
  * Parse the client request headers and build request object.
 */
-func parseRequest(r *bufio.Reader) (request *Request, err os.Error) {
-  return new(Request), nil
+func parseRequest(r *textproto.Reader) (*Request, os.Error) {
+
+  // create new request object
+  request := new(Request)
+
+  methodLine, _ := r.ReadLine()
+  methodLineElements := strings.Split(methodLine, " ")
+  
+  if (len(methodLineElements) != 3) {
+    return request, os.NewError("Invalid request")
+  }
+
+  request.Method = methodLineElements[0]
+  request.Path = methodLineElements[1]
+  request.HTTPVersion = methodLineElements[2]
+  return request, nil
 }
-
-
